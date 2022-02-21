650cd0ed1fad6a1a3180e47dd5fa3db66a2d7282
Alexey Kokukhin <akokuhin@inno.co>
2022-02-21 16:53:11 +0500

    updated Ckeditor::AplicationController

--- app/controllers/ckeditor/application_controller.rb
+++ app/controllers/ckeditor/application_controller.rb
@@ -1,20 +1,20 @@
 class Ckeditor::ApplicationController < Ckeditor.parent_controller.constantize
   layout 'ckeditor/application'
 
-  before_filter :find_asset, :only => [:destroy]
-  before_filter :ckeditor_authorize!
-  before_filter :authorize_resource
+  before_action :find_asset, only: [:destroy]
+  before_action :ckeditor_authorize!
+  before_action :authorize_resource
 
   protected
 
-    def respond_with_asset(asset)
-      _response = Ckeditor::AssetResponse.new(asset, request)
-      _callback = ckeditor_before_create_asset(asset)
+  def respond_with_asset(asset)
+    response = Ckeditor::AssetResponse.new(asset, request)
+    callback = ckeditor_before_create_asset(asset)
 
-      if _callback && asset.save
-        render _response.success(config.relative_url_root)
-      else
-        render _response.errors
-      end
+    if callback && asset.save
+      render response.success(config.relative_url_root)
+    else
+      render response.errors
     end
+  end
 end
